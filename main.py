be8ad9370e5eb0c240948c9c6ae3cf7e25bd41a5
bluehyena <42088479+bluehyena@users.noreply.github.com>
2021-01-04 18:16:13 +0900

    [Fix] - Delete main

--- main.py
+++
@@ -1,6 +0,0 @@
-from instapy import InstaPy
-import env
-
-session = InstaPy(username=env.instagramId, password=env.instagramPassword)
-session.login()
-session.like_by_tags(["hunger", "unicef", "vegan"], amount=5)
