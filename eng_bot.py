6dbd273f519a35d068c0bde74d567bafae74fc76
bluehyena <42088479+bluehyena@users.noreply.github.com>
2021-01-05 15:17:04 +0900

    [Fix] - Fix instagram bots

--- eng_bot.py
+++ eng_bot.py
@@ -10,7 +10,7 @@ session.set_relationship_bounds(enabled=True,
 				   max_followers=1000,
 				    max_following=449000,
 				     min_followers=30,
-				      min_following=70)
+				      min_following=100)
 
 session.set_skip_users(skip_private=True,
                            skip_no_profile_pic=True)
@@ -19,13 +19,13 @@ session.set_do_follow(True, percentage=100, times=2)
 
 session.like_by_tags(['#instafood', '#vegfood', '#healthyfood', '#veganrecipes', '#vegetarianrecipes','#veganlife'], amount=50)
 
-session.set_comments(["Good!, I love this!", 
-    "Nice!!", "Excellent :)", "Perfect :)", "Great!!", "Amazing!!", 
-    "Beautiful :)", "Wonderful!!", "Good job", "pretty good.", "It looks great!", 
-    "You must be a man of ability.", "I like this.", "Good.", "Sweet!", 
-    "Beautiful :heart_eyes:"
-    ])
-session.set_do_comment(True, percentage=40)
+# session.set_comments(["Good!, I love this!", 
+#     "Nice!!", "Excellent :)", "Perfect :)", "Great!!", "Amazing!!", 
+#     "Beautiful :)", "Wonderful!!", "Good job", "pretty good.", "It looks great!", 
+#     "You must be a man of ability.", "I like this.", "Good.", "Sweet!", 
+#     "Beautiful :heart_eyes:"
+#     ])
+# session.set_do_comment(True, percentage=40)
 
 
 session.set_dont_like(['selfi','naked','nsfw'])
